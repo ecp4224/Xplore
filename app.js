9a08ac8ea0493ccabc9a7ab422a98c3812f3f7d3
Eddie Penta <ecp4224@gmail.com>
2015-02-21 17:33:12 -0500

    Moved index.js -> app.js

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.post('/feed', function(req, res) {
         tags: req.body.tags
     };
 
-    events.createEvent(eventObject, username, function(post) {
+    events.createEvent(eventObject, createUser, function(post) {
         res.status(200).send(JSON.stringify(post));
     }, function (err) {
         res.status(500).send(err);
@@ -71,14 +71,14 @@ app.post('/feed', function(req, res) {
 });
 
 app.post('/feed/:postId', function(req, res){
-    var postId = req.params.name;
+    var postId = req.params.postId;
     if (!postId) {
-        req.sendStatus(500);
+        res.status(500).send("No post id!");
         return;
     }
 
     events.eventAction(postId, req.body.actionType, req.body.data, username, function() {
-        res.status(200);
+        res.status(200).end();
     }, function(e) {
         res.status(500).send(e);
     });
