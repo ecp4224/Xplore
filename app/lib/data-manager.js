10457edab92e48cee4ca2b704a8d7d38abfec172
robby <rabaya@gmail.com>
2015-02-21 14:05:57 -0500

    adding the data manager

--- app/lib/data-manager.js
+++ app/lib/data-manager.js
@@ -1,6 +1,32 @@
+var Firebase = require('firebase');
+var myRootRef = new Firebase('https://scorching-inferno-8193.firebaseio.com');
+
 module.exports.getUserWithUserName = function(username, callback) {
-	var rabayaRef = myRootRef.child('/users/rabaya');
-	rabayaRef.on('value', function(snapshot) {
-		console.log()
-	});
+    var userRef = new Firebase('https://scorching-inferno-8193.firebaseio.com/users/'+username);
+	if (userRef) {
+		userRef.on('value', function(snapshot) {
+			if (snapshot) {
+				callback(null, snapshot.val());
+			} else {
+				callback(null, null);
+			}
+		});
+	} else {
+		callback( null, null );
+	}
+}
+
+module.exports.getPostById = function(postId, callback) {
+	var postRef = new Firebase('https://scorching-inferno-8193.firebaseio.com/posts/'+postId);
+	if (postRef) {
+		postRef.on('value', function(snapshot) {
+			if (snapshot) {
+				callback(null, snapshot.val());
+			} else {
+				callback(null, null);
+			}
+		});
+	} else {
+		callback( null, null );
+	}
 }
