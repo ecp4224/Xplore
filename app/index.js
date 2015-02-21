4f7ae5fb248e1f2f139d27a76a7c98a66ea03b2b
Eddie Penta <ecp4224@gmail.com>
2015-02-21 13:02:30 -0500

    Started serving /feed`

---
+++ app/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.render('')
+});
+
+var TEST_JSON = "[{\"id\":0,\"title\":\"Chess Club\",\"short_description\":\"Chess club meeting tuesday!\",\"description\":\"We are having a chess club meeting this thrusday and we're looking for people!\",\"date\":\"2/26/2015\",\"time\":\"3:00PM\",\"tickets\":[{\"role\":\"coach\",\"taken\":1},{\"role\":\"score_keeper\",\"taken\":0}]},{\"id\":0,\"title\":\"Chess Club\",\"short_description\":\"Chess club meeting tuesday!\",\"description\":\"We are having a chess club meeting this thrusday and we're looking for people!\",\"date\":\"2/26/2015\",\"time\":\"3:00PM\",\"tickets\":[{\"role\":\"coach\",\"taken\":1},{\"role\":\"score_keeper\",\"taken\":0}]},{\"id\":0,\"title\":\"Chess Club\",\"short_description\":\"Chess club meeting tuesday!\",\"description\":\"We are having a chess club meeting this thrusday and we're looking for people!\",\"date\":\"2/26/2015\",\"time\":\"3:00PM\",\"tickets\":[{\"role\":\"coach\",\"taken\":1},{\"role\":\"score_keeper\",\"taken\":0}]}]";
+app.get('/feed', function(req, res) {
+    if (!req.query.type) {
+        res.sendStatus(500);
+        res.send("Invalid request");
+        return;
+    }
+    var type = req.query.type;
+
+    //TODO Get feed based on type
+    res.send(TEST_JSON);
+});
+
+app.listen(80);
