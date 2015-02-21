acbaae10fddc7563c650e6334f1f96297686b99d
robby <rabaya@gmail.com>
2015-02-21 14:31:16 -0500

    Ability to add posts to database

--- app/lib/data-manager.js
+++ app/lib/data-manager.js
@@ -30,3 +30,33 @@ module.exports.getPostById = function(postId, callback) {
 		callback( null, null );
 	}
 }
+
+module.exports.addPost = function(post, callback) {
+    var postRef = new Firebase('https://scorching-inferno-8193.firebaseio.com/posts');
+    if (postRef) {
+        postRef.on('value', function(snapshot) {
+            if (snapshot) {
+                var posts = snapshot.val();
+                var postCountRef = new Firebase('https://scorching-inferno-8193.firebaseio.com/post_count');
+                postCountRef.on('value', function( postCountSnapshot ) {
+                    var postCount = parseInt( postCountSnapshot.val() );
+                    post.id = postCount;
+                    posts[postCount.toString()] = post;
+                    postRef.set(posts, function( error ) {
+                        if( error ) {
+                            callback( error );
+                        } else {
+                            callback( null, post );
+                        }
+                    });
+                });
+
+            } else {
+                var err = new Error("Missing snapshot");
+                callback(err, null);
+            }
+        });
+    } else {
+        callback( null, null );
+    }
+}
