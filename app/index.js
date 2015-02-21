4612cd5a31c7857590a876b84b4f34e5fa6724ff
Eddie Penta <ecp4224@gmail.com>
2015-02-21 13:05:45 -0500

    Serve home.hbs

--- app/index.js
+++ app/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.set('views', '/views');
+app.set('view engine', 'hbs');
+
 app.get('/', function(req, res) {
-    res.render('')
+    res.render('home');
 });
 
 var TEST_JSON = "[{\"id\":0,\"title\":\"Chess Club\",\"short_description\":\"Chess club meeting tuesday!\",\"description\":\"We are having a chess club meeting this thrusday and we're looking for people!\",\"date\":\"2/26/2015\",\"time\":\"3:00PM\",\"tickets\":[{\"role\":\"coach\",\"taken\":1},{\"role\":\"score_keeper\",\"taken\":0}]},{\"id\":0,\"title\":\"Chess Club\",\"short_description\":\"Chess club meeting tuesday!\",\"description\":\"We are having a chess club meeting this thrusday and we're looking for people!\",\"date\":\"2/26/2015\",\"time\":\"3:00PM\",\"tickets\":[{\"role\":\"coach\",\"taken\":1},{\"role\":\"score_keeper\",\"taken\":0}]},{\"id\":0,\"title\":\"Chess Club\",\"short_description\":\"Chess club meeting tuesday!\",\"description\":\"We are having a chess club meeting this thrusday and we're looking for people!\",\"date\":\"2/26/2015\",\"time\":\"3:00PM\",\"tickets\":[{\"role\":\"coach\",\"taken\":1},{\"role\":\"score_keeper\",\"taken\":0}]}]";
